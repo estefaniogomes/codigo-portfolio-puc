d476fdd9a592dbef4c4d2ffa56ec0ce8cb226053
Estefanio Farias <estefanio.farias@SINTEL.LOCAL>
2025-02-21 08:36:50 -0300

    first commit

---
+++ 03-minibrowser/Map.js
@@ -0,0 +1,17 @@
+//TDD - Test Driven Delevopment
+
+class HistoryMap {
+
+}
+
+var novoMap = new Map();
+
+// 11/11/2011 as 11 da noite
+novoMap.set('11/11/2011 - 23:30', { title: 'Titulo do site', url: 'url do site' });
+novoMap.set('11/11/2011 - 23:30', {});
+novoMap.set('11/11/2011 - 23:30', {});
+novoMap.set('11/11/2011 - 23:30', {});
+novoMap.set('11/11/2011 - 23:31', {});
+
+var resultado = 'olá pessoas';
+console.log(resultado); Map { '11/11/2011 - 23:30 - URL' => }
